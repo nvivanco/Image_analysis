30dcffc9e7aeb58f3428b470a1684b9131a9e1ed
Nora <nora.vivanco@gmail.com>
2024-12-02 16:00:32 -0800

    catered pre processing for duplex mother machine images captured with 1x1 pixel binning

--- pipeline_pre_process_example.py
+++ pipeline_pre_process_example.py
@@ -2,16 +2,28 @@ import pre_process_mm
 
 
 if __name__ == '__main__':
-	root_dir = '/Users/noravivancogonzalez/Desktop/20240919/dimm_giTG66_glucose_1'
 
-	drift_corrected_path = pre_process_mm.napari_ready_format_drift_correct(root_dir, 'dimm', c = 0)
-	drift_corrected_path = '/Users/noravivancogonzalez/Desktop/20240919/dimm_giTG66_glucose_1/hyperstacked/drift_corrected'
-	path_to_rotated_images = pre_process_mm.rotate_stack(drift_corrected_path, c=0, growth_channel_length=280)
+
+	root_dir = '/Users/noravivancogonzalez/Desktop/20241114'
+
+	drift_corrected_path = pre_process_mm.drift_correct(root_dir, 'DuMM', c=0)
+
+	path_to_rotated_images = pre_process_mm.rotate_stack(drift_corrected_path, c=0, growth_channel_length=400)
+	# growth channel length depends on # of pixels, which is affected by binning
+	# had to change rotation angle to not be negative since growth channels were facing downward
+
+	#path_to_hyperstacked = '/Users/noravivancogonzalez/Desktop/20241001/dimm_CL000_1/only_analyze_these/hyperstacked/drift_corrected/rotated'
+
+	#pre_process_mm.unstack_tcyx_to_cyx(path_to_hyperstacked)
+
+	#path_to_rotated_images = '/Users/noravivancogonzalez/Desktop/20241114/DuMM_CL001_CL006_glucose/hyperstacked/drift_corrected/rotated'
+
 	path_to_mm_channels = pre_process_mm.extract_mm_channels(path_to_rotated_images)
+
 
 	FOV = '003'
 	empty_stack_id = '692'
 	ana_peak_ids = ['41', '68', '95']
 	phase_channel = 0
 
-	pre_process_mm.subtract_fov_stack(path_to_mm_channels, FOV, empty_stack_id, ana_peak_ids, phase_channel)
+	#pre_process_mm.subtract_fov_stack(path_to_mm_channels, FOV, empty_stack_id, ana_peak_ids, phase_channel)
