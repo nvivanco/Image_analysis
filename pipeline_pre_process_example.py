3ea1708e693c044004f56b8e7b31a5deed126b9d
Nora <nora.vivanco@gmail.com>
2025-01-23 16:51:47 -0800

    updated tutorial

--- pipeline_pre_process_example.py
+++ pipeline_pre_process_example.py
@@ -1,28 +1,26 @@
 import pre_process_mm
 
+root_dir = '/Users/noravivancogonzalez/Desktop/20241114'
+
+#drift_corrected_path = pre_process_mm.drift_correct(root_dir, 'DuMM', c=0)
+
+drift_corrected_path = '/Users/noravivancogonzalez/Desktop/20241114/hyperstacked/drift_corrected'
+
+#path_to_rotated_images = pre_process_mm.rotate_stack(drift_corrected_path, c=0, growth_channel_length=400)
+# growth channel length depends on # of pixels, which is affected by binning
+# had to change rotation angle to not be negative since growth channels were facing downward
+
+#path_to_mm_channels = pre_process_mm.extract_mm_channels(path_to_rotated_images)
+
+path_to_mm_channels = '/Users/noravivancogonzalez/Desktop/20241114/hyperstacked/drift_corrected/rotated/mm_channels'
+
+
+FOV = '001'
+empty_stack_id = '707'
+ana_peak_ids = ['1164', '1314', '2227']
+channel = 1
 
 if __name__ == '__main__':
-
-
-	root_dir = '/Users/noravivancogonzalez/Desktop/20241114'
-
-	#drift_corrected_path = pre_process_mm.drift_correct(root_dir, 'DuMM', c=0)
-
-	drift_corrected_path = '/Users/noravivancogonzalez/Desktop/20241114/hyperstacked/drift_corrected'
-
-	#path_to_rotated_images = pre_process_mm.rotate_stack(drift_corrected_path, c=0, growth_channel_length=400)
-	# growth channel length depends on # of pixels, which is affected by binning
-	# had to change rotation angle to not be negative since growth channels were facing downward
-
-	#path_to_mm_channels = pre_process_mm.extract_mm_channels(path_to_rotated_images)
-
-	path_to_mm_channels = '/Users/noravivancogonzalez/Desktop/20241114/hyperstacked/drift_corrected/rotated/mm_channels'
-
-
-	FOV = '001'
-	empty_stack_id = '707'
-	ana_peak_ids = ['1164', '1314', '2227']
-	channel = 1
 
 	pre_process_mm.subtract_fov_stack(path_to_mm_channels, FOV, empty_stack_id, ana_peak_ids, method = 'fluor', channel_index= channel)
 
