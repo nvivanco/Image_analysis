8e8995e9d4dda9de25b79668e5acfecbde99d4c0
acJuarez <82752259+acJuarez@users.noreply.github.com>
2025-05-08 10:43:21 -0700

    start of a script with helper functions that may be used elsewhere in code, currently just one that assists in getting the folder path of a directory

--- HF.py
+++ HF.py
@@ -1,16 +1,24 @@
+"""""
+The following is a set of helper functions (HF) that assist in streamlining code
+elsewhere
+"""""
+
 import tkinter as tk
 from tkinter import filedialog
 
 
+""""
+the following function is used to open up a gui window that allows 
+the user to select a directory and returns the path of the directory selected 
+""""
 def select_directory():
     root = tk.Tk()
     root.withdraw()
     folder_path = filedialog.askdirectory(title = "Select experiment")
-
     if folder_path:
-        print("path_selected")
-        print(folder_path)
+        print("path_selected: " + folder_path)
         return folder_path
     else:
         print("No folder was selected")
         return None
+ 
